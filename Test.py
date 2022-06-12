4f609c2d7bce9485fc92c578784638258aeec76e
Adom7 <72967617+Adom7@users.noreply.github.com>
2022-06-12 15:41:34 +0200

    Few Tests

--- Test.py
+++ Test.py
@@ -1,1 +1,20 @@
+from ast import If
+
+
 print('Hello World !')
+
+User = input('Choose Option 1 or 2 ')
+
+
+def sfy(option):
+    if option == "1":
+        return print("You Chose Option 1")
+
+    elif option == "2":
+        return print('Oprion 2')
+
+    else:
+        print('Enter a correct Option')
+
+
+sfy(User)
