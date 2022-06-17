34c7c757d45e2c5c526342503db880c7f0c2a204
Adom7 <72967617+Adom7@users.noreply.github.com>
2022-06-17 17:09:53 +0200

    Recaptcha doing a good job

--- Indeed_Bot.py
+++ Indeed_Bot.py
@@ -42,10 +42,10 @@ time.sleep(1)
 Password_Input.send_keys(Indeed_Password)
 time.sleep(1)
 Password_Input.send_keys(Keys.ENTER)
-time.sleep(60)
+time.sleep(20)
 # Can't find a way to resolve the captcha or avoid it , so it's the time to reply to the captcha Myself before letting the bot take control
 # ANCHOR
-# I Should try to connect via google ?
+# I Should try to connect via google ? google Connnexion not allowed in these chrome pages
 Close_Pop_Up = driver.find_element_by_class_name('popover-x-button-close')
 Close_Pop_Up.click()
 time.sleep(1)
@@ -68,5 +68,45 @@ On Indeed 2 major types of job propositions
     The One that continues on Indeed --
 '''
 
-# 'Postuler sur indeed' -- find_element_by_id('indeedApplyButton')
-# "Continuer pour postuler" -- find_element_by_class_name('jobsearch-CallToApply-applyButton-newDesign')
+
+# Really frustating to work on Indeed, Recaptcha is doing a good job
+
+for job in All_Jobs:
+    print('Job Found')
+    time.sleep(1)
+    job.click()
+    time.sleep(1)
+    try:
+        driver.find_element_by_id('indeedApplyButton').click()
+        print('ID')
+        driver.find_element_by_css_selector('.css-zv0ejl').click()
+        print('css')
+        driver.find_element_by_class_name(' css-zv0ejl e8ju0x51').click()
+        print('class')
+        driver.find_element_by_name('indeedApplyButton').click()
+        print('name')
+        # A New page is loaded
+        time.sleep(2)
+        Progress_Bar = driver.find_element_by_class_name('ia-ProgressBar')
+        print(Progress_Bar.get_attribute('value'))
+        if Progress_Bar.get_attribute('value') == 100:
+            Submit_Button = driver.find_element_by_class_name(
+                '.ia-continueButton')
+            Submit_Button.click()
+            print('Applied to the job')
+            # TODO Improvement : Add the title , salary and the location of the job
+        else:
+            print('Multiple Steps , Aborted')
+            Abort_button = driver.find_element_by_css_selector(
+                '.ia-pageButtonGroup-exit')
+            Abort_button.click()
+            time.sleep(1)
+            Confirm_Button = driver.find_element_by_css_selector('.css-r6nywx')
+            Confirm_Button.click()
+            time.sleep(1)
+    except NoSuchElementException:
+        print("Job Post Outside of LinkedIn, Aborted")
+    continue
+
+
+# Can't work on it atm , verification code long to arrive , too many requests. i'll come back to it
